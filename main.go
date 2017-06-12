67d857059da3cde63c0d779cf3df448fa0f74601
edo1z <edo1z3145@gmail.com>
2017-06-13 00:02:45 +0900

    add view

--- main.go
+++ main.go
@@ -3,12 +3,25 @@ package main
 import (
 	"github.com/kataras/iris"
 	"github.com/kataras/iris/context"
+	"github.com/kataras/iris/view"
 )
 
 func main() {
 	app := iris.New()
-	app.Handle("GET", "/", func(ctx context.Context) {
-		ctx.WriteString("hello world!")
+	app.AttachView(view.HTML("./views", ".html").Reload(true))
+
+	app.Handle("GET", "/", top)
+	app.Handle("GET", "/about", func(ctx context.Context) {
+		ctx.View("about.html")
 	})
-	app.Run(iris.Addr(":8000"))
+	app.Handle("GET", "/users", func(ctx context.Context) {
+		ctx.HTML("<p>this is users page.</p>")
+	})
+
+	app.Run(iris.Addr(":8080"), iris.WithCharset("UTF-8"))
+}
+
+func top(ctx context.Context) {
+	ctx.ViewData("Username", "Taro")
+	ctx.View("top.html")
 }
